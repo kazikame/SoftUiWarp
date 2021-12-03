2ec2a551460f221b3cae3f943f4d1957eb5b061f
Saksham Goel <saksham47@hotmail.com>
2021-12-02 22:22:14 -0600

    okay for real this time

--- perftest/write_lat.cpp
+++ perftest/write_lat.cpp
@@ -44,70 +44,58 @@ int write_lat_init(perftest_context *perftest_ctx, uint32_t lstag, struct send_d
     write_wr.wr.rdma.rkey = sd->stag;
     write_wr.wr.rdma.remote_addr = sd->offset;
     if (!perftest_ctx->is_client) {
-        // Fill the server's region with incrementing values.
+        // Fill the client's region with incrementing values.
         for (int i = 0; i < perftest_ctx->buf_size; i++) {
             perftest_ctx->buf[i] = (char) i;
         }
+    } else {
+        memset(perftest_ctx->buf, 0, perftest_ctx->buf_size);
     }
     return 0;
 }
 
 int write_lat_iter(perftest_context *perftest_ctx) {
     int ret;
-    // Client clears buffer and waits to receive write.
-    if (perftest_ctx->is_client) {
-        lwlog_debug("waiting for write at ...");
-        do { _mm_clflush(&perftest_ctx->buf[1]); } while (perftest_ctx->buf[1] == 0) ;
-        lwlog_debug("found write!");
-        perftest_ctx->buf[1]++;
-#ifdef PERFTEST_TEST
-        for (int i = 0; i < perftest_ctx->buf_size; i++) {
-            if (perftest_ctx->buf[i] != ((char) i)) {
-                lwlog_err("Received incorrect value 0x%hhx for index %d!", perftest_ctx->buf[i], i);
+    //  Server waits to receive write.
+    bool role = perftest_ctx->is_client;
+    do {
+        if (role) {
+            lwlog_debug("waiting for write ...");
+            if (perftest_ctx->is_client) {
+                do { _mm_clflush(&perftest_ctx->buf[1]); } while (perftest_ctx->buf[1] == 0) ;
+            } else {
+                do { _mm_clflush(&perftest_ctx->buf[1]); } while (perftest_ctx->buf[1] == 1) ;
+            }
+            lwlog_debug("found write!");
+            // Only clear a byte if we're benchmarking.
+            if (perftest_ctx->is_client) {
+                perftest_ctx->buf[1] = (char) 0;
+            } else {
+                perftest_ctx->buf[1] = (char) 1;
+            }
+        } else {
+            // Write the buffer to the remote.
+            lwlog_debug("issuing write ...");
+            ret = rdmap_write(perftest_ctx->ctx, write_wr);
+            if (ret < 0) {
+                lwlog_err("Failed to issue RDMA Write!");
+                return -1;
+            }
+            lwlog_debug("completed write");
+            struct work_completion wc;
+            auto write_cq = perftest_ctx->ctx->send_q->cq->q;
+            do { ret = write_cq->try_dequeue(wc); } while (!ret) ;
+            lwlog_debug("received completion");
+            if (wc.status != WC_SUCCESS) {
+                lwlog_err("Received remote send with error");
+                return -1;
+            } else if (wc.opcode != WC_WRITE) {
+                lwlog_err("Received wrong message type!");
                 return -1;
             }
         }
-#endif // PERFTEST_TEST
-    }
-
-    // Write the buffer to the remote.
-    lwlog_debug("issuing write ...");
-    ret = rdmap_write(perftest_ctx->ctx, write_wr);
-    if (ret < 0) {
-        lwlog_err("Failed to issue RDMA Write!");
-        return -1;
-    }
-    lwlog_debug("completed write");
-    struct work_completion wc;
-    auto write_cq = perftest_ctx->ctx->send_q->cq->q;
-    do { ret = write_cq->try_dequeue(wc); } while (!ret) ;
-    lwlog_debug("received completion");
-    if (wc.status != WC_SUCCESS) {
-        lwlog_err("Received remote send with error");
-        return -1;
-    } else if (wc.opcode != WC_WRITE) {
-        lwlog_err("Received wrong message type!");
-        return -1;
-    }
-
-    // Server clears buffer and waits to receive write.
-    if (!perftest_ctx->is_client) {
-        lwlog_debug("waiting for write ...");
-        do { _mm_clflush(&perftest_ctx->buf); } while (perftest_ctx->buf[1] == 1) ;
-        lwlog_debug("found write!");
-    }
-    /* Prepare for next iteration. */
-    if (perftest_ctx->is_client) {
-#ifdef PERFTEST_TEST
-        // Clear the whole buffer if we're testing.
-        memset(perftest_ctx->buf, 0, perftest_ctx->buf_size);
-#else
-        // Only clear a byte if we're benchmarking.
-        perftest_ctx->buf[1] = (char) 0;
-#endif
-    } else {
-        perftest_ctx->buf[1] = (char) 1;
-    }
+        role = !role;
+    } while (role != perftest_ctx->is_client);
     return 0;
 }
 
